370c78d429ee031ce1e9a2da2aa413c222ad43ab
Ann <anka2_9grudovaya@mail.ru>
2022-10-18 21:44:45 +0300

    add Task15

--- Task15/Program.cs
+++ Task15/Program.cs
@@ -2,25 +2,23 @@
 //и проверяет, является ли этот день выходным. 
 // 6 -> да
 // 7 -> да
-// 1 -> нет
+// 1 -> нет 
 
 Console.Write("Введите число: ");
 int num = Convert.ToInt32(Console.ReadLine());
 
 if (num > 0 && num < 8)
 {
-
     bool dayWeek = DayWeek(num);
     Console.WriteLine(dayWeek ? "Да" : "Нет");
 }
 else
 {
-
     Console.WriteLine("Такого дня недели не существует");
 }
 
 bool DayWeek(int number)
- {
+{
 
-     return number > 5;
- }
+    return number > 5;
+}
