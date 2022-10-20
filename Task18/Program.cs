ea78467aafc95673ee4343ca5c7edbd34b654b58
Ann <anka2_9grudovaya@mail.ru>
2022-10-20 22:25:19 +0300

    add Task18

--- Task18/Program.cs
+++ Task18/Program.cs
@@ -8,16 +8,16 @@ int num = Convert.ToInt32(Console.ReadLine());
 
 string Quarter(int number)
 {
-    if (number == 1) return "xc > 0 && yc > 0";
-    if (xc < 0 && yc > 0) return 2;
-    if (xc < 0 && yc < 0) return 3;
-    if (xc > 0 && yc < 0) return 4;
+    if (number == 1) return "xc > 0 и yc > 0";
+    if (number == 2) return "xc < 0 и yc > 0";
+    if (number == 3) return "xc < 0 и yc < 0";
+    if (number == 4) return "xc > 0 и yc < 0";
     return null;
 }
 
 string quarter = Quarter(num);
 
-Console.WriteLine(quarter == null
-            ? $"Введены некорректные координаты"
-            : "Указанные координаты соответствуют четверти -> {quarter}");
+Console.WriteLine(quarter != null 
+                    ? $"Указанные координаты соответствуют четверти -> {quarter}"
+                    :"Введены некорректные координаты");
 
