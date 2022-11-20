6081caa78b3fc1b9e55b8504848ea3bee7263cfd
Ann <anka2_9grudovaya@mail.ru>
2022-11-20 23:56:08 +0300

    add Task50

---
+++ Task50/Program.cs
@@ -0,0 +1,70 @@
+﻿// Задача 50. 
+// Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 1, 7 -> такого элемента в массиве нет
+
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns]; // 0, 1
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],5} | ");
+            else Console.Write($"{matrix[i, j],5}");
+        }
+        Console.WriteLine(" | ");
+    }
+}
+
+
+void FindArrElemntIndex(int[,] matrix, int row, int column)
+
+{
+    if (row < matrix.GetLength(0) && column < matrix.GetLength(1))
+    {
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                if (i == row && j == column)
+                {
+                    Console.WriteLine($" Элемент с позициями [{row}, {column}] = {matrix[i, j]} ");
+                }
+            }
+        }
+    }
+    else Console.WriteLine($" Элемента с позициями [{row}, {column}] в массиве нет! ");
+
+}
+
+Console.WriteLine("Внимание! Нумерация элементов массива начинается с 0! ");
+Console.Write("Введите индекс строки массива: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите индекс столбца массива: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int[,] array2D = CreateMatrixRndInt(3, 4, -10, 10);
+PrintMatrix(array2D);
+FindArrElemntIndex(array2D, rows, columns);
