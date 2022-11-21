3669dc3978782283d30e04fa3a16ae89dfc8c01a
Ann <anka2_9grudovaya@mail.ru>
2022-11-21 23:14:36 +0300

    add Task52

--- Task52/Program.cs
+++ Task52/Program.cs
@@ -42,33 +42,24 @@ void PrintMatrix(int[,] matrix)
 
 double[] AverageColumns(int[,] matrix)
 {
-    int sum = 0;
+
+    double sum = 0;
     double avr = 0;
-    double a = 0;
     double[] result = new double[matrix.GetLength(1)];
     for (int j = 0; j < matrix.GetLength(1); j++)
     {
+        sum = 0;
         for (int i = 0; i < matrix.GetLength(0); i++)
         {
             sum += matrix[i, j];
         }
         avr = (sum / matrix.GetLength(0));
-        Console.WriteLine(avr);
-        a = avr;
-        Console.WriteLine(Math.Round(a,2));
-        result[j] = Math.Round(avr, 2);
-        Console.WriteLine(result[j]);
-
-        // x = (b2 - b1) / (k1 - k2);
-        // y = k2 * x + b2;
-        // double[] result = { Math.Round(x, 2), Math.Round(y, 2) };
-        // return result;
-
-        //result[j] = Math.Round(sum / matrix.GetLength(0), 1);
+        result[j] = Math.Round(avr, 1);
     }
 
     return result;
 }
+
 
 void PrintArrayDouble(double[] array)
 {
@@ -81,7 +72,8 @@ void PrintArrayDouble(double[] array)
     Console.WriteLine("]");
 }
 
-int[,] array2D = CreateMatrixRndInt(3, 4, -10, 10);
+
+int[,] array2D = CreateMatrixRndInt(3, 4, 0, 10);
 PrintMatrix(array2D);
 double[] averageColumns = AverageColumns(array2D);
 Console.WriteLine();
