b6ea55ee672aa922508695c27237de92d4f8138b
Ann <anka2_9grudovaya@mail.ru>
2022-11-24 18:05:53 +0300

    add Task60

---
+++ Task60/Program.cs
@@ -0,0 +1,60 @@
+﻿// Задача 60.
+// Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+// Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Например, задан массив размером 2 x 2 x 2.
+// Результат:
+// 66(0,0,0) 27(0,0,1) 25(0,1,0) 90(0,1,1)
+// 34(1,0,0) 26(1,0,1) 41(1,1,0) 55(1,1,1)
+
+int[,,] CreateMatrixRndInt(int rows, int columns, int depth, int min, int max)
+{
+    int[,,] matrix = new int[rows, columns, depth];
+    int m = 10;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+               matrix[i, j, k] = m;
+               m++;
+            }
+
+        }
+    }
+    return matrix;
+}
+
+
+void PrintMatrix(int[,,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                if (k < matrix.GetLength(2))
+                {
+                    Console.Write($"{matrix[i, j, k],6} ({i},{j},{k}) ");
+                }
+            }
+
+        }
+        Console.WriteLine("");
+    }
+}
+
+
+Console.Write("Введите количество строк массива: m = ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов массива: n = ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите глубину массива: l = ");
+int l = Convert.ToInt32(Console.ReadLine());
+
+int[,,] matrix = CreateMatrixRndInt(m, n, l, 10, 100);
+PrintMatrix(matrix);
+
+
