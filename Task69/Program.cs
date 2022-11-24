4fabc840022910edbecfa93fb127fb4ef69b15de
Ann <anka2_9grudovaya@mail.ru>
2022-11-24 21:36:56 +0300

    add Task69

--- Task69/Program.cs
+++ Task69/Program.cs
@@ -4,4 +4,22 @@
 // A = 3; B = 5 -> 243 (3⁵)
 // A = 2; B = 3 -> 8
 
+int Degree(int number1, int number2)
+{
+    if (number2 == 0) return 1;
+    return number1 * Degree(number1, number2 - 1);
+}
+
+
+Console.Write("Введите натуральное число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите натуральное число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num2 >= 0)
+{
+    int degree = Degree(num1, num2);
+    Console.Write($"Число {num1} в степени {num2} = {degree}");
+}
+else Console.Write("Введите натуральную степень числа");
 
